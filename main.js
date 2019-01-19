d31657b1cf41bc1881b941728919a8aef1f55649
Michael Kwok <michael.kwok00@gmail.com>
2019-01-19 15:06:07 -0700

    fixed eslintrc, linted js files

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require("electron");
 
-function createWindow () {
-  // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600 })
+function createWindow() {
+    // Create the browser window.
+    let win = new BrowserWindow({ width: 800, height: 600 });
 
-  // and load the index.html of the app.
-  win.loadFile('index.html')
+    // and load the index.html of the app.
+    win.loadFile("index.html");
+
+    win.on("closed", () => {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        win = null;
+    });
+
 }
 
-app.on('ready', createWindow)
+app.on("ready", createWindow);
