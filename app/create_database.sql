d9609675a0dcb801c9a2f358cc8b4d847a803178
armiantos <sumitro@ualberta.ca>
2019-01-19 15:32:15 -0700

    added sql files

--- app/create_database.sql
+++ app/create_database.sql
@@ -1,8 +1,8 @@
 CREATE TABLE Term(
     termID int,
     termTitle varchar[79],
-    endDate date,
-    startDate date
+    endDate text,
+    startDate text
 )
 
 CREATE TABLE Course(
@@ -18,5 +18,36 @@ CREATE TABLE Course(
     faculty varchar[80],
     facultyCode varchar[10],
     subjectTitle varchar[80],
-    
+    units float
+)
+
+CREATE TABLE CourseClass(
+    component text,
+    section text,
+    classLocation text,
+    classDay text,
+    startTime text,
+    endTime text,
+    class int,
+    courseID, int,
+    term int,
+    capacity int,
+    classStatus text,
+    enrollStatus text,
+    startDate text,
+    endDate text,
+    examDate text,
+    examStartTime text,
+    examEndTime text,
+    examLocation text,
+    examStatus text,
+    campus text,
+    classNotes text,
+    classType text,
+    consent text,
+    gradingBasis text,
+    instructionMode text,
+    instructorID text,
+    courseSession text,
+    units float   
 )
