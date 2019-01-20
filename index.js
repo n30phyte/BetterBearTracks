4252b120339106f2eb748a4ae51e18814b59eb5e
Michael Kwok <michael.kwok00@gmail.com>
2019-01-19 19:36:28 -0700

    added dynamic search on html page

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 $(() => {
-    const electron = require('electron')
-    console.log("loaded")
-    $('#courseSearch')
-        .keyup(function() {
-            console.log();
-            updateList(query);
-        })
+    const electron = require("electron");
+    console.log("loaded");
+    $("#courseSearch")
+        .keydown(function(event) {
+            if (event.which == 13) {
+                event.preventDefault();
+            }
+            updateList(event);
+        });
 });
 
 function updateList(query) {
+    let courses = ["ECE 202", "ECE 210", "ECE 212"];
 
+    let output = [];
+
+    courses.forEach(function(course) {
+        if (course.includes(query.target.value)) {
+            output.push(course);
+        }
+    });
+
+    printList(output);
+}
+
+function printList(courses) {
+    $("#searchResult").empty();
+    courses.forEach(function(course) {
+        $("#searchResult").append("<a href=\"#\" class=\"course-item list-group-item list-group-item-action\">" + course + "</a>");
+    });
 }
