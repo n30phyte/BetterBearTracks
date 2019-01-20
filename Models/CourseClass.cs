13f7da328c9954a9ab041c9d87fc228209cdf686
armiantos <armiantos@gmail.com>
2019-01-20 08:50:03 -0700

    models + scraper

--- Models/CourseClass.cs
+++ Models/CourseClass.cs
@@ -4,32 +4,32 @@
 namespace BetterBearTracks.Models
 {
 	public class CourseClass {
-		string component;
-		string section;
-		string location;
-		string day;
-		DateTime startTime;
-		DateTime endTime;
-		int classID;
-		int courseID;
-		int term;
-		int capacity;
-		char classStatus;
-		char enrollStatus;
-		DateTime startDate;
-		DateTime endDate;
-		DateTime examDate;
-		DateTime examTime;
-		string examLocation;
-		string examStatus;
-		string campus;
-		string classNotes;
-		string classType;
-		string consent;
-		string gradingBasis;
-		string instructionMode;
-		string instructorID;
-		string session;
-		float units;
+		public string component { get; set; }
+		public string section { get; set; }
+		public string location { get; set; }
+		public string day { get; set; }
+		public DateTime startTime { get; set; }
+		public DateTime endTime { get; set; }
+		public int classID { get; set; }
+		public int courseID { get; set; }
+		public int term { get; set; }
+		public int capacity { get; set; }
+		public char classStatus { get; set; }
+		public char enrollStatus { get; set; } 
+		public DateTime startDate { get; set; }
+		public DateTime endDate { get; set; }
+		public DateTime examDate { get; set; }
+		public DateTime examTime { get; set; }
+		public string examLocation { get; set; }
+		public string examStatus { get; set; }
+		public string campus { get; set; }
+		public string classNotes { get; set; }
+		public string classType { get; set; }
+		public string consent { get; set; }
+		public string gradingBasis { get; set; }
+		public string instructionMode { get; set; }
+		public string instructorID { get; set; }
+		public string session { get; set; }
+		public float units { get; set; }
 	}
 }
---
+++ Models/Scraper.cs
@@ -0,0 +1,15 @@
+﻿namespace BetterBearTracks.Models {
+
+	public class Scraper {
+		private DirectoryEntry dirEntry;
+		private DirectorySearcher dirSearch;
+
+		public Scraper() {
+			dirEntry = new DirectoryEntry("ldap://directory.srv.ualberta.ca");
+			dirSearch = new DirectorySearcher(dirEntry)
+			{
+				pageSize = 
+			}
+		}
+	}
+}
